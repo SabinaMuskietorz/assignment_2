716a4dfc5422a508b963a314680c16a590444dea
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2020-03-09 13:43:29 +0000

    first commit

--- styles/main.js
+++ styles/main.js
@@ -1,7 +1,28 @@
+$( document ).ready(function() {
+
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".hamburger" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".hamburger" ).hide();
+  $( ".cross" ).show();
+  });
+  });
+  
+  $( ".cross" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".cross" ).hide();
+  $( ".hamburger" ).show();
+  });
+  });
+  
+  });
+
+
 jQuery("input, textarea").blur(function(){
     if (jQuery(this).val()!="") {
       jQuery(this).addClass("active");
     } else {
       jQuery(this).removeClass("active");
     }
-  })
+  });
