58d27a594663c9427a71c9f678146632d89a4bb9
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2020-04-10 12:38:44 +0100

    first commit

--- main.js
+++ main.js
@@ -21,9 +21,9 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/portfoliopic21.png');
+      myImage.setAttribute ('src','images/pic2.jpg');
     } else {
-      myImage.setAttribute ('src','images/pic2.png');
+      myImage.setAttribute ('src','images/portfoliopic21.jpg');
     }
 }
 
