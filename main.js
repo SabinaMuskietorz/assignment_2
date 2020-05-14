75641fd93c5657e49cb2d1adfb6eb25a953d581d
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2020-05-14 15:25:03 +0100

    first commit

--- main.js
+++ main.js
@@ -28,19 +28,29 @@ myImage.onclick = function () {
   }
 }
 //quote array
-let quotes = ["Blank", "\"Either I will find a way, or I will make one.\" - Philip Sidney", 
-"\"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.\"- Thomas A. Edison", 
-"\"You are never too old to set another goal or to dream a new dream.\"- C.S Lewis", "\"If you can dream it, you can do it.\"- Walt Disney", 
-"\"Never give up, for that is just the place and time that the tide will turn.\"- Harriet Beecher Stowe", 
-"\"I know where I'm going and I know the truth, and I don't have to be what you want me to be. I'm free to be what I want.\"- Muhammad Ali", 
-"\"If you always put limit on everything you do, physical or anything else. It will spread into your work and into your life. There are no limits. There are only plateaus, and you must not stay there, you must go beyond them.\"- Bruce Lee",];
-let randNum = Math.floor(Math.random() * 7) + 1;
+let quotes = ["\" Either I will find a way, or I will make one. - Philip Sidney \"", 
+"\"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.Thomas A. Edison\"", 
+"\"You are never too old to set another goal or to dream a new dream.- C.S Lewis\"", 
+"\"If you can dream it, you can do it.- Walt Disney \"", 
+"\"Never give up, for that is just the place and time that the tide will turn.- Harriet Beecher Stowe\"", 
+"\"I know where I'm going and I know the truth, and I don't have to be what you want me to be. I'm free to be what I want. Muhammad Ali\"", 
+"\" If you always put limit on everything you do, physical or anything else. It will spread into your work and into your life. There are no limits. There are only plateaus, and you must not stay there, you must go beyond them.- Bruce Lee\"",];
+
 
 function genQuote() {
-  
- let genQuote = document.getElementById(".quote");
- //randNum ++;
- genQuote.src = quotes[randNum];
+      
+let randNum = Math.floor((Math.random() * 6) + 1);
+
+let genQuote = document.querySelector(".quote");
+
+ //genQuote.src = quotes[randNum];
+
+
+
+ console.log(randNum);
+
+ console.log(quotes[randNum]);
+
 }
 
 
